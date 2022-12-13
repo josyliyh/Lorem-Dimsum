7bc0769e69a6b88c31c7b0bab5fef1585f089540
Josy Li <josyliyh@gmail.com>
2022-12-12 16:38:38 -0800

    created app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {data} from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount < 0 ) amount =1;
+    if (amount > 11 ) amount = 11;
+    setText(data.slice (0,amount));
+    return;
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lorem Dimsum</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Paragraph:</label>
+        <input 
+        type="number" 
+        name='amount' 
+        id='amount'
+        value={count}
+        onChange={(e) => setCount(e.target.value)} 
+        />
+        <button className='btn'>Generate</button>
+      </form>
+      <article>
+        {text.map((data, index) => (
+          <p key={index}>{data}</p>
+        ))}
+      </article>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const data = [
+    'happy',
+    'sad',
+    'lovely'
+];
