9a19401f76a91d59f2bc7b73132f9009c9d1367e
Josy Li <josyliyh@gmail.com>
2022-12-14 15:45:03 -0800

    rand sentence

--- src/App.js
+++ src/App.js
@@ -5,36 +5,78 @@ import {data} from './data';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [option, setOption] = useState(0);
+  const [sen, setSen] = useState([]);
+  const [para, setPara] = useState([]);
   const [text, setText] = useState([]);
 
+  const defaultProps = {
+    p: 1,
+    avgWordsPerSentence: 8,
+    avgSentencesPerParagraph: 8,
+    startWithLoremIpsum: true,
+    random: true,
+  };
+  
   const handleSubmit =(e) => {
     e.preventDefault();
+    let text = '';
     let amount = parseInt(count);
     if (amount < 0 ) amount =1;
-    if (amount > 11 ) amount = 11;
-    setText(data.slice (0,amount));
-    return;
+    let i = 0;
+    let sentence = '';
+    // setText(data.slice (0,amount));
+    // data = data.sort(() => 0.5 - Math.random());
+    
+    // setText(data.sort(() => 0.5 - Math.random()).slice (0,(Math.random() * (15 - 7) + 7)));
+    while (i < amount) {
+      // sentence.slice(1);
+      sentence =(sentence + (data.sort(() => 0.5 - Math.random()).slice (0,(Math.random() * (15 - 7) + 7))));
+      i = i + 1;
+    };
+    setText(sentence);
+    // setText(text + (data.sort(() => 0.5 - Math.random()).slice (0,(Math.random() * (15 - 7) + 7))));
+
+    // return;
+    // if(count > 2000){
+    //   count = 2000; // max words which can be generated is 2000 only
+    //   } else if(count < 1 ){
+    //       count = 5; // min words is 5 
+    //   } 
+
     
   };
+  
+  
 
   return (
     <div className="App">
       <h1>Lorem Dimsum</h1>
       <form onSubmit={handleSubmit}>
-        <label>Paragraph:</label>
+
         <input 
         type="number" 
-        name='amount' 
-        id='amount'
+        name="amount" 
+        id="amount"
         value={count}
         onChange={(e) => setCount(e.target.value)} 
         />
-        <button className='btn'>Generate</button>
+
+        <select name = "options"
+        onChange={(e) => setOption(e.target.value)} >
+          <option value = "paragraphs">paragraphs</option>
+          <option value = "sentences">sentences</option>
+        </select>
+        <button className="btn">Generate</button>
       </form>
       <article>
-        {text.map((data, index) => (
-          <p key={index}>{data}</p>
-        ))}
+        <div>
+        {/* {text.map((data, index) => (
+          <div className='dimsum' key={index}>{data}</div>
+        ))} */}
+        {text}
+        
+        </div>
       </article>
     </div>
   );
